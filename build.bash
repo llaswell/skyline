59bb68a0d5b406281ec54cf455ac26abeaf60926
llaswell <llaswell@outland2.(none)>
2015-11-08 17:52:22 -0800

    build creates bin dir if doesn't exist

--- build.bash
+++ build.bash
@@ -1,5 +1,9 @@
  #!/bin/bash
 
+if [ ! -d "./bin" ]; then
+	mkdir ./bin
+fi
+
 #g++ -std=gnu++11 -ocreateBuildings createBuildings.cpp
 g++ -std=c++0x -o bin/createBuildings createBuildings.cpp
 g++ -o bin/skyline skyline.cpp
