d6694febfba376bffe6c13e0ffa7753cad74fe92
EC2 Default User <ec2-user@ip-172-31-46-101.us-west-2.compute.internal>
2015-11-09 02:20:25 +0000

    Add 2mm and 10mm tiers for benchmarking

--- genFiles.bash
+++ genFiles.bash
@@ -2,7 +2,7 @@
 
 echo "Files will be written to data/"
 
-for i in 10000 15000 30000 50000 75000 100000 1000000
+for i in 10000 15000 30000 50000 75000 100000 1000000 2000000 10000000
 do
 	FILENAME=${i}_buildings.dat
 	echo "Writing file: " $FILENAME
--- time.bash
+++ time.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in 10000 15000 30000 50000 75000 100000 1000000
+for i in 10000 15000 30000 50000 75000 100000 1000000 2000000 10000000
 do
 	FILENAME=./data/${i}_buildings.dat
 	echo "Processing " $FILENAME
